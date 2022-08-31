2ff41fe98c59048583ea8fd2cfb52a396f3d76f6
DanielBrandon <112131739+DanielBrandon@users.noreply.github.com>
2022-08-31 01:20:40 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/************************************************************
+* Java Program Name
+*************************************************************
+* Daniel Brandon
+* Created: 08/25/2022
+CMSC 255 Section 001
+*************************************************************/
+
+public class Hello
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Name: Daniel");
+		System.out.println("");
+		System.out.println("File Name: Hello.java");
+		System.out.println("");
+		System.out.println("Program Description: 'Hello' program");
+		System.out.println("");
+		System.out.println("Created: 08/25/2022");
+		System.out.println("");
+		System.out.println("Class: CMSC 255 Section 001");
+		System.out.println();
+
+	}
+}
